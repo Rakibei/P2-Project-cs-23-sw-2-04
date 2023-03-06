36a013c62d66b022927311aa6ecaaefbb9a61eb5
Markus Kjær Pedersen <50503934+CowsenF@users.noreply.github.com>
2023-03-06 11:04:36 +0100

    small fix

--- Database/database.js
+++ Database/database.js
@@ -8,48 +8,63 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise();
+    database: process.env.MYSQL_DATABASE,
+}).promise()
 
 export async function getUsers() {
-    const [rows] = await pool.query("SELECT * FROM users")
-    return rows
+    try {
+        const [rows] = await pool.query("SELECT * FROM users")
+        return rows
+    } catch (error) {
+        console.log(error)
+        return false // error occurred
+    }
 }
 
 export async function getUser(id) {
-    const [user] = await pool.query(`
-    SELECT * 
-    FROM users
-    WHERE id = ?
-    `,[id])
-    return user[0]
+    try {
+        const [user] = await pool.query(`
+        SELECT * 
+        FROM users
+        WHERE id = ?
+        `,[id])
+        return user[0]
+    } catch (error) {
+        console.log(error)
+        return false // error occurred
+    }
 }
 
 export async function createUser(username, password) {
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
 
-    const [result] = await pool.query(`
-    INSERT INTO users (username, password)
-    VALUES (?, ?)
-    `, [username, hash])
-    const id = result.insertId
-    return getUser(id)
+        const [result] = await pool.query(`
+        INSERT INTO users (username, password)
+        VALUES (?, ?)
+        `, [username, hash])
+        const id = result.insertId
+        return getUser(id)
+    } catch (error) {
+        console.log(error)
+        return false // error occurred
+    }
 }
 
 
 export async function comparePassword(username, password) {
     try {
-        const [rows] = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
-        const user = rows[0];
+        const [rows] = await pool.query('SELECT * FROM users WHERE username = ?', [username])
+        const user = rows[0]
         if (!user) {
-            return false; // user not found
+            return false // user not found
         }
-        const match = await bcrypt.compare(password, user.password);
-        return match; // true or false
+        const match = await bcrypt.compare(password, user.password)
+        return match // true or false
     } catch (error) {
-        console.log(error);
-        return false; // error occurred
+        console.log(error)
+        return false // error occurred
     }
 }
 /*
