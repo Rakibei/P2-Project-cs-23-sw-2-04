a43e152780452ef3261e5c6ec3271b30f96d0f04
Markus Kjær Pedersen <50503934+CowsenF@users.noreply.github.com>
2023-03-13 11:18:50 +0100

    Database fix

--- database.js
+++ database.js
@@ -1,17 +1,24 @@
-const mysql = require('mysql2')
-const bcrypt = require('bcrypt')
+import { createPool } from 'mysql2'
+import { genSalt, hash as _hash, compare } from 'bcrypt'
+import dotenv from 'dotenv'
 
-const dotenv = require('dotenv')
-dotenv.config()
+export function connectToDatabase() {
+    try {
+        dotenv.config()
+        const pool = createPool({ 
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD,
+            database: process.env.MYSQL_DATABASE,
+        }).promise()
+        return pool;
+    } catch {
+        console.log(error)
+        return false // error occurred
+    }
+}
 
-const pool = mysql.createPool({ 
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-}).promise()
-
-async function getUsers() {
+export async function getUsers(pool) {
     try {
         const [rows] = await pool.query("SELECT * FROM users")
         return rows
@@ -21,7 +28,7 @@ async function getUsers() {
     }
 }
 
-async function getUser(id) {
+export async function getUser(pool, id) {
     try {
         const [user] = await pool.query(`
         SELECT * 
@@ -35,10 +42,10 @@ async function getUser(id) {
     }
 }
 
-async function createUser(username, password) {
+export async function createUser(pool, username, password) {
     try {
-        const salt = await bcrypt.genSalt(10)
-        const hash = await bcrypt.hash(password, salt)
+        const salt = await genSalt(10)
+        const hash = await _hash(password, salt)
 
         const [result] = await pool.query(`
         INSERT INTO users (username, password)
@@ -52,48 +59,47 @@ async function createUser(username, password) {
     }
 }
 
-async function comparePassword(username, password) {
+export async function comparePassword(username, password) {
     try {
         const [rows] = await pool.query('SELECT * FROM users WHERE username = ?', [username])
         const user = rows[0]
         if (!user) {
             return false // user not found
         }
-        const match = await bcrypt.compare(password, user.password)
+        const match = await compare(password, user.password)
         return match // true or false
     } catch (error) {
         console.log(error)
         return false // error occurred
     }
 }
+
+export async function createProject(pool, ProjectId, name, startDate, endDate, hoursSpent) {
+    try {
+        const values = [ProjectId, name, startDate, endDate, hoursSpent];
+        await pool.query('INSERT INTO projects (ProjectId, name, startDate, endDate, hoursSpent) VALUES (?, ?, ?, ?, ?)', values);
+        return true; // success
+    } catch (error) {
+        console.log(error);
+        return false; // error occurred
+    }
+}
+
+
+
+
+
+
 /*
 comparePassword('madstest', 'hej').then((result) => {
     console.log(result); // true or false
 });
 */
 
-
-
-
-
-
-
-
-
-
-
 //const newUser = await createUser('madstest', 'hej')
 //console.log(newUser)
-
-const promise1 = new Promise((resolve, reject) => {
-    resolve( getUsers());
-});
-
-promise1.then((users) => {
-    console.log(users)
-})
-
-//const users = getUsers().promise()
+//const pool = await connectToDatabase();
+//const users = await getUsers(pool)
 //console.log(users)
 
 //const user =  await getUser(2)
