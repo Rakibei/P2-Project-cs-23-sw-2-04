61dfa0064a805edebccfb0ef3e78d892d110f405
NFage <nfje22@student.aau.dk>
2023-03-28 09:28:19 +0100

    CreatePDF has a build in promise

--- pdf/pdfTest.js
+++ pdf/pdfTest.js
@@ -1,17 +1,24 @@
+//This version of CreatePDF include an additional Promise. For whatever reason it seemed
+//that the original return function passed before the PDF had been created, which caused the path
+//used in Server.js to get a non-functional path. now the path should only be passed on after the PDF
+//has been created.
 import PDFDocument from 'pdfkit';
 import fs from 'fs';
 
 export async function CreatePDF(username,projectData){
+    return new Promise((resolve) => { // The
 
-    const doc = new PDFDocument();
-    doc.pipe(fs.createWriteStream("./pdf/"+username+"TimeSheet.pdf"));
-    doc.text("Time data for "+ username)
-    doc.text(JSON.stringify(projectData));
-    doc.end();
+        const doc = new PDFDocument();
+        const stream = doc.pipe(fs.createWriteStream('./pdf/'+username+'TimeSheet.pdf'));
 
-    return "./pdf/"+username+"TimeSheet.pdf";
+        stream.on('finish', () => {
+            resolve("./pdf/"+username+"TimeSheet.pdf");
+        });
 
-}
+        doc.fontSize(18).text("Allocation of time for "+ username);
 
-
+        doc.fontSize(12).text(JSON.stringify(projectData));
+        doc.end();
+    });
+};
 
