931502fceefb0e2ff20351cc33b3620397dea83a
Kyneb98 <tkwa22@student.aau.dk>
2023-04-18 13:51:37 +0200

    Email notification  on admin page

--- database/databaseUser.js
+++ database/databaseUser.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { GetManagerProjects } from "./databaseProject";
+import { GetManagerProjects } from "./databaseProject.js";
 
 export async function GetUsers(pool) {
     try {
