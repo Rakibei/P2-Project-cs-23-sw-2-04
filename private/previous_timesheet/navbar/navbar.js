7a858998c42695c85658382654f23322b80e5550
CowsenF <50503934+CowsenF@users.noreply.github.com>
2023-05-21 16:58:19 +0200

    Comments

--- private/previous_timesheet/navbar/navbar.js
+++ private/previous_timesheet/navbar/navbar.js
@@ -1,54 +1,62 @@
 window.addEventListener('load', () => {
-
-    fetch("/sesionData")
+  // Make an HTTP request to "/sesionData" endpoint
+  fetch("/sesionData")
     .then((response) => response.json())
     .then((data) => {
+      // Retrieve the UserLevel data from the response
       UserLevel = data.UserLevel;
   
-      if(UserLevel.isAdmin){
-      AdminButton = document.getElementById("NavAdmin").style.display = "block";
-      }
-      if(UserLevel.isManager){
-      ManagerButton = document.getElementById("NavManager").style.display = "block";
-      }
-      if(UserLevel.isProjectManager){
-      ProjectManagerButton = document.getElementById("NavProjectManager").style.display = "block";  
+      // Check if the user is an admin and display the corresponding button
+      if (UserLevel.isAdmin) {
+        AdminButton = document.getElementById("NavAdmin").style.display = "block";
       }
   
+      // Check if the user is a manager and display the corresponding button
+      if (UserLevel.isManager) {
+        ManagerButton = document.getElementById("NavManager").style.display = "block";
+      }
+  
+      // Check if the user is a project manager and display the corresponding button
+      if (UserLevel.isProjectManager) {
+        ProjectManagerButton = document.getElementById("NavProjectManager").style.display = "block";
+      }
     });
 });
 
-function openMenu(){
-    document.getElementById("menu-icon").classList.toggle('fa-bars');
-    document.getElementById("menu-icon").classList.toggle('fa-x');
-    document.getElementById("menu").classList.toggle('menu-open');
-    document.getElementById("menu").classList.toggle('menu-closed');
+function openMenu() {
+  // Toggle the 'fa-bars' and 'fa-x' classes on the menu-icon element
+  document.getElementById("menu-icon").classList.toggle('fa-bars');
+  document.getElementById("menu-icon").classList.toggle('fa-x');
+
+  // Toggle the 'menu-open' and 'menu-closed' classes on the menu element
+  document.getElementById("menu").classList.toggle('menu-open');
+  document.getElementById("menu").classList.toggle('menu-closed');
 }
 
 window.onload = function(){
     document.getElementById("menu-text").addEventListener( 'click', openMenu);
 }
 
-document.getElementById("NavLogout").addEventListener("click",() => {
+document.getElementById("NavLogout").addEventListener("click", () => {
+  // Create a data object with the functionName property set to "Logout"
+  const data = {
+    functionName: "Logout"
+  };
 
-    const data = {
-        functionName: "Logout"
+  // Make a POST request to '/userRequests' endpoint with the data as JSON payload
+  fetch('/userRequests', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => {
+    // Check if the response was redirected
+    if (response.redirected) {
+      // Redirect the user to the specified response URL
+      window.location.href = response.url;
     }
-
-    fetch('/userRequests', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      })
-      .then(response => {
-        if (response.redirected) {
-            window.location.href = response.url;
-          }
-    
-      })
-      .catch(error => console.error(error));
-
-
+  })
+  .catch(error => console.error(error));
 });
